1127741098aa15e010dbb3041f4f803fd617a38d
enzoesper <enzoesper@hotmail.com>
2020-11-28 19:29:43 -0300

    adding a useEffect hook for closing the dropdown when the user clicks outside the dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   
   // state for manage the open/close of the dropdown
   const [open, setOpen] = useState(false);
+  // ref to the most parent element for the dropdown component
+  const ref = useRef();
+
+  // runs one time, only when the component renders on the screen (argument with [])
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      
+      /* THIS LOGIC ALLOWS TO CLOSE THE DROPDOWN WHEN THE USER CLICKS OUTSIDE THE DROPDOWN */
+      // if the element that was clicked is inside the dropdown most partent element, then 
+      // return early
+      if (ref.current.contains(event.target)) {
+        return;
+      };
+      // otherwise (if the element that was clicked is outside the dropdown most parent element), then
+      // te dropdown is closed
+      setOpen(false);
+    });
+  }, []);
+
 
   const renderedOptions = options.map((option) => {
     // filtering the option list for not to show an item that is selected twice in the list
@@ -17,9 +36,9 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       </div>
     );
   });
-  
+
   return(
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>

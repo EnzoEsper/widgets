1545f6437aeec2951488ef38b819f2539c656c83
enzoesper <enzoesper@hotmail.com>
2020-11-28 02:34:56 -0300

    filtering the option list for not to show an item that is selected twice in the list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
+
+    // filtering the option list for not to show an item that is selected twice in the list
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
       <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
         {option.label}

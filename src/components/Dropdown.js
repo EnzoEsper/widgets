90ffeb6491bc250671e5c1d4d8f6d807e94f878c
enzoesper <enzoesper@hotmail.com>
2020-11-28 02:11:19 -0300

    displayinh the dropdown with the options passed from the app component as props

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Dropdown = () => {
-  return <div>Dropdown!</div>
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    )
+  });
+  
+  return(
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          {/* i tag is not really for an icon, its just a convention that semantic ui makes use of for show an icon inside an i tag */}
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;

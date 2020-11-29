e9615fcc9fb257b00e9435d4ac8c3341b1fea359
enzoesper <enzoesper@hotmail.com>
2020-11-28 21:28:34 -0300

    removing comments

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,12 +11,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   useEffect(() => {
 
     const onBodyClick = (event) => {
-
-      console.log(`BODY CLICKED!`);
       /* THIS LOGIC ALLOWS TO CLOSE THE DROPDOWN WHEN THE USER CLICKS OUTSIDE THE DROPDOWN */
       // if the element that was clicked is inside the dropdown most partent element, then return early
       // The null propagation operator allows to avoid the error when the toggle botton is pressed and
-      // ref.current becomes null because the dropdown was deleted 
+      // ref.current becomes null because the dropdown was deleted (first is called the event handler and last the clean up function)
       if (ref.current?.contains(event.target)) {
         return;
       };
@@ -30,7 +28,6 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     // this will we invoked right before the next time that this function (useEffect) is called AND when we stop showing the 
     // dropdown component to the screen
     return () => {
-      console.log('Clean up called!');
       document.body.removeEventListener('click', onBodyClick);
     };
   }, []);

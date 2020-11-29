33c8fdb6a4decc3e6182c53a34af90916c420298
enzoesper <enzoesper@hotmail.com>
2020-11-28 22:26:47 -0300

    scaffolding the Translate component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -31,23 +32,27 @@ const options = [
     label: "A shade of Blue",
     value: "blue"
   }
-]
+];
+
+
 
 const App = () => {
 
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowdropdown] = useState(true);
+  // const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowdropdown] = useState(true);
 
   return (
     <div>
-      <button onClick={() => setShowdropdown(!showDropdown)}>Toggle Dropdown</button>
+
+      <Translate />
+      {/* <button onClick={() => setShowdropdown(!showDropdown)}>Toggle Dropdown</button>
       {showDropdown ?
         <Dropdown
           options={options}
           selected={selected}
           onSelectedChange={setSelected}
         /> : null
-      }
+      } */}
       
     </div>
   );
